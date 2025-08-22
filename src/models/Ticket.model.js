e507ef4bccb79229dc57a1104f9c80990fbb8683
pankaj-deepnapsoftech <itsybizz@gmail.com>
2025-08-22 15:06:30 +0530

    some change

--- src/models/Ticket.model.js
+++ src/models/Ticket.model.js
@@ -5,7 +5,7 @@ const TicketSchema = new Schema(
     department: { type: Schema.Types.ObjectId, ref: "Department" },
     title: { type: String, required: true },
     description: { type: String },
-    ticket_id: { type: String, unique: true },
+    ticket_id: { type: String, unique: true, },
     due_date: { type: Date, required: true },
     creator: { type: Schema.Types.ObjectId, ref: "User", required: true },
     priority: {type: String,required: true,default: "Medium",enum: ["High", "Low", "Medium"]},
