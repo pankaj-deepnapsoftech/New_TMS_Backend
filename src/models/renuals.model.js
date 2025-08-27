128852a172814ac64603e961624f25f91c6d44d4
pankaj-deepnapsoftech <itsybizz@gmail.com>
2025-08-27 13:14:02 +0530

    some change

--- src/models/renuals.model.js
+++ src/models/renuals.model.js
@@ -3,7 +3,7 @@ import {Schema,model} from "mongoose";
 const RenualSchema = new Schema({
     customer:{type:String,required:true},
     renual_date:{type:Date,required:true},
-    Product:{type:String,required:true},
+    product:{type:String,required:true},
 },{timestamps:true});
 
 export const RenualModel = model("Renual",RenualSchema);
