0e72e0f80eb1c29b2a089fb5631eaaa0f3e52a4b
pankaj-deepnapsoftech <itsybizz@gmail.com>
2025-08-29 11:39:29 +0530

    some change

--- src/middleware/UserLogoutHandler.js
+++ src/middleware/UserLogoutHandler.js
@@ -2,12 +2,15 @@ import { StatusCodes } from "http-status-codes";
 
 // ---------------------- local import here --------------------
 import { FindByUserWithId } from "../Services/User.services.js";
-import { NotFoundError } from "../utils/CoustomError.js";
+import { BadRequestError, NotFoundError } from "../utils/CoustomError.js";
 import { VerifyToken } from "../utils/TokenHandler.js";
 
 
 export const LogoutUserIfLoginAnyWere = async (req, res, next) => {
-    const token = req.cookies.rt
+    const token = req?.cookies?.rt
+    if(!token){
+        throw new BadRequestError("Token must be provide","LogoutUserIfLoginAnyWere function")
+    }
     const { id } = VerifyToken(token);
     const user = await FindByUserWithId(id);
     if (!user) {
