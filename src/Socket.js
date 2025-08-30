9f4899aedc105b33c65cd6bb222bf7578224f2bb
pankaj-deepnapsoftech <itsybizz@gmail.com>
2025-08-30 17:53:53 +0530

    some change

--- src/Socket.js
+++ src/Socket.js
@@ -1,14 +1,13 @@
 import { Server } from "socket.io"
 
 // ----------------------------------- local imports here -------------------------------
-import { config } from "./config/env.config.js"
+// import { config } from "./config/env.config.js"
 
 export const SocketConnection = async (server) => {
     const io = new Server(server, {
         cors: {
-            origin: config.NODE_ENV === "development" ? config.LOCAL_CLIENT_URL : config.CLIENT_URL,
+            origin: "*",
             methods: ["POST", "PUT", "PATCH", "DELETE", "OPTION","GET"],
-            credentials: true
         }
     })
 
